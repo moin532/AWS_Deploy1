40e4c015136d44e45d3203cd1d4b54f1d422946e
Mohammed moin <moinmisba92@gmail.com>
2024-12-08 14:11:49 +0530

    first and last

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    port: 80,
-  },
+  plugins: [react()],
 });
